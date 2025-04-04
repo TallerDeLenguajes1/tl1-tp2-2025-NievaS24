371be1cf074754b129230a8785d997aa4cd1f591
NievaS24 <santinieva24@gmail.com>
2025-04-03 21:20:06 -0300

    eleccion de procesador al azar

--- tp2_4.c
+++ tp2_4.c
@@ -11,12 +11,21 @@ struct {
 
 int main() {
     srand(time(NULL));
+    char tipos[6][10] = {
+        "Intel",
+        "AMD",
+        "Celeron",
+        "Athlon",
+        "Core",
+        "Pentium"};
     Compu computadoras[5];
     for (int i = 0; i < 5; i++) {
         computadoras[i].velocidad = 1 + rand() % 3;
         computadoras[i].anio = 2015 + rand() % 10;
         computadoras[i].cantidad_nucleos = 1 + rand() % 8;
-        printf("%d\n", computadoras[i].cantidad_nucleos);
+        int tipo_azar = rand() % 6;
+        computadoras[i].tipo_cpu = &tipos[tipo_azar][0];
+        printf("%s\n", computadoras[i].tipo_cpu);
     }
     return 0;
 }
