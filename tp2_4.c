1ebac35801e302d8b7d321715aa1fe73732a8c25
NievaS24 <santinieva24@gmail.com>
2025-04-03 21:27:28 -0300

    listarPcs agregado

--- tp2_4.c
+++ tp2_4.c
@@ -9,6 +9,17 @@ struct {
     char *tipo_cpu;
 } typedef Compu;
 
+void listarPCs(Compu pcs[], int cantidad) {
+    for (int i = 0; i < cantidad; i++) {
+        printf("Computadora %d: \n\n", i + 1);
+        printf("Velocidad: %d \n", pcs[i].velocidad);
+        printf("Año de fabricacion: %d \n", pcs[i].anio);
+        printf("Cant. de nucleos: %d \n", pcs[i].cantidad_nucleos);
+        printf("Procesador: %s \n", pcs[i].tipo_cpu);
+        printf("\n\n");
+    }
+}
+
 int main() {
     srand(time(NULL));
     char tipos[6][10] = {
@@ -25,7 +36,7 @@ int main() {
         computadoras[i].cantidad_nucleos = 1 + rand() % 8;
         int tipo_azar = rand() % 6;
         computadoras[i].tipo_cpu = &tipos[tipo_azar][0];
-        printf("%s\n", computadoras[i].tipo_cpu);
     }
+    listarPCs(computadoras, 5);
     return 0;
 }
